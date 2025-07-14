abca9bd8631c9678960bb81bf5ad1579836a6fbb
nataliafedorovabi <140884272+nataliafedorovabi@users.noreply.github.com>
2025-07-14 21:23:48 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -123,6 +123,10 @@ async def get_goal(update: Update, context: CallbackContext):
     # ДОБАВЛЕНО: Отправка файлов из соответствующей папки
     folder_path = f"menus/{calories}"
     if os.path.exists(folder_path):
+        await context.bot.send_message(
+        chat_id=query.message.chat_id,
+        text="Или попробуй меню на 7 дней:"
+        )
         files_sent = 0
         for filename in os.listdir(folder_path):
             if files_sent >= 3:
